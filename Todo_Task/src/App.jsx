4a01ab653438cefd1fd7fcf69114f6ad9c4bfee1
JijoAndrews <jijoandrews07@gmail.com>
2024-10-01 07:31:02 +0530

    updated files

--- Todo_Task/src/App.jsx
+++ Todo_Task/src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import './Components/Todoheader'
+import Cardbody from './Components/Cardbody'
 
 function Bsicapp()
 {
@@ -23,17 +24,77 @@ function Bsicapp()
   )
 }
  
+const cardlist=[];
+
+function createacard(data,inputdata)
+{
+  console.log("updated data:",data);  
+  console.log("input data:",inputdata);
+//  cardlist.push(<Cardbody key={data.id}  id={data.id} name={data.name} dis={data.discript}/>);
+
+  return <Cardbody key={data.id}  id={data.id} name={data.name} dis={data.discript}/>
+  //return cardlist;
+}
+
+
+function testcompt(){
+  console.log("testingggg")
+  return(
+  <>
+    <h2>Todo Application dawwwwwwwwwwwwwww</h2>
+  </>
+  )
+}
+
+const Tododata=[[{}],0];
+
+let datacount=0;
+
 function App() {
+
   const [count, setCount] = useState(0)
 
+  const data={id:"noid",name:"john doe",discript:"well this is intial discription",totalcount:0}
+  const [inputdata,setdata]= useState(data);
+  const [odata,setodata] =useState([{}]);
+
+  useEffect(()=>{
+    console.log("rerender:",odata);
+  },[odata])
+
+  // const handlenamechange=(e)=>{  console.log("Name:",e.target.id,e.target.value) ; setdata(data.name=e.target.value)}
+  // const handledischange=(e)=>{  console.log("Discription:",e.target.id,e.target.value) ; setdata(data.name=e.target.value)}
+
+  let inputname="";
+  let inputdiscript="";
+  const handlebothinput=(namedata,disdata)=>{   
+    if(namedata && disdata)
+      {
+        const Tododata1={};
+        Tododata1.id=datacount;
+        Tododata1.name=namedata;
+        Tododata1.discript=disdata;
+    
+        console.log("current input:",Tododata1,odata);
+        setodata(odata=>[...odata,Tododata1]);
+        datacount=datacount+1;  
+      }else
+      {
+        alert("dont leve empty inputs");
+      }
+    };
   return (
-    <div className="titleclass">
+
+    <>
       <h1>Todo Application</h1>
-      <input type='text' placeholder='Todo Name'></input>
-      <input type='text' placeholder='Todo Discription'></input>
-      <button onClick={() => setCount((count) => count + 1)}>Enter</button>
-      <p className='owntext'>count is {count}</p>
-    </div>
+      <div key={"tc"} className="titleclass">
+     
+        <input type='text' placeholder='Todo Name' id='defaultname' onChange={(e)=>{inputname=e.target.value}}></input>
+        <input type='text' placeholder='Todo Discription' id='defaultid' onChange={(e)=>inputdiscript=e.target.value}></input>
+        <button onClick={() =>handlebothinput(inputname,inputdiscript)}>Add Todo test</button>
+      </div>
+    </>
+   
   )
 }
 
--- Todo_Task/src/Components/Cardbody.jsx
+++ Todo_Task/src/Components/Cardbody.jsx
@@ -1,21 +1,44 @@
 import React from 'react'
+import { useState,useEffect} from 'react'
 import App from '../App'
-const Cardbody = () => {
+
+
+function createcard(id,name,discrip)
+{
+    // const [count, setCount] = useState(0)
+    console.log("clicked-createcard func");
   return (
-    <div className='row'>
-        <App/>
-        <App/>
-        <App/>
-        <App/>
-        <App/>
-        <App/>
-        <App/>
-        <App/>
-        <App/>
-        <App/>
-        <App/>
-        <App/>
+    <div key={id} className='col-lg-4'>
+      <div className='col-sm-12'>
+        <h1>Vite + React</h1>
+        <div className="card">
+
+        <button onClick={() =>{console.log("clicked")}}>
+            id is {id}
+            name is {name}
+            discrip is {discrip}
+        </button>
+
+        {/* <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+        </button> */}
+        
+        </div>
+      </div>   
     </div>
+   
+  )
+
+}
+
+
+const Cardbody = ({dataid,dataname,datadriscp}) => {
+  console.log("clicked-entered card")
+  return (
+   <>
+   {/* {createcard(dataid,dataname,datadriscp)} */}
+   <div key={dataid}> {dataid} hello there buddy</div>
+   </>
   )
 }
 
